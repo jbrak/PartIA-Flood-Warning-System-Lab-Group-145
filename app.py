812225222292a00075c4fc49c77c0612248d06f0
jbrak <joe@brakoniecki.com>
2025-01-25 18:14:35 +0000

    Added properties to MonitoringStation and wrote a basic shiny app to show all the locations of the flood monitoring stations on.

---
+++ app.py
@@ -0,0 +1,24 @@
+"""Script to produce shiny dashboard with markers for each station"""
+
+# Run with ```run shiny```
+from ipyleaflet import Map, MarkerCluster, CircleMarker
+from shiny.express import ui
+from shinywidgets import render_widget
+from floodsystem.stationdata import build_station_list
+
+stations = build_station_list()
+
+ui.h2("Map Showing Flood Station Locations")
+
+@render_widget
+def map():
+    """Function to draw map of all flood stations"""
+    map = Map(center=(52.3555, 1.1743), zoom=6)
+
+    markers = tuple((CircleMarker(location = i.coord, radius = 3, color = "red") for i in stations))
+
+    marker_cluster = MarkerCluster(markers = markers, max_cluser_radius = 10)
+
+    map.add_layer(marker_cluster)
+
+    return map
